876fc18cce4a567d2978307892b7d656dba5dc07
thz <thz@163.cn>
2023-05-06 10:13:38 +0800

    thz master 4 modify

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -9,5 +9,6 @@ public class App {
         System.out.println("Hello Git! thz 1 first modify!");
         System.out.println("Hello Git! thz 2 first modify!");
         System.out.println("Hello Git! thz 3 first modify!");
+        System.out.println("Hello Git! thz 4 first modify!");
     }
 }
