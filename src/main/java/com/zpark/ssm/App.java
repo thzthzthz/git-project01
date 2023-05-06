ee58db19ab2849dfab36ed7b60fbaff89bf5e769
thzthzthz <132049274+thzthzthz@users.noreply.github.com>
2023-05-06 16:48:10 +0800

    Update App.java

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -9,6 +9,6 @@ public class App {
         System.out.println("Hello Git! thz 1 first modify!");
         System.out.println("Hello GitHub! thz master first push GitHub!");
         System.out.println("Hello GitHub! thz master first1 push GitHub!");
-        System.out.println("Test pull fetch from Github!");
+        System.out.println("Test fetch pull from Github!");
     }
 }
