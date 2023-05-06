1feb4a465975ed54452dd71af0696ca69f02b72e
thz <thz@163.cn>
2023-05-06 10:04:16 +0800

    thz master first modify

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -5,5 +5,6 @@ public class App {
         System.out.println("Hello.Git!");
         System.out.println("Hello.Git! thz master second commit!");
         System.out.println("Hello Git! thz hotfix first commit!");
+        System.out.println("Hello Git! thz master first modify!");
     }
 }
