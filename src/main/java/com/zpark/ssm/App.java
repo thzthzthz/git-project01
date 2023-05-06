53fd3a34daf7a84d05913e8bcf741c32c91b05cf
thz <thz@163.cn>
2023-05-06 10:02:34 +0800

    1

---
+++ src/main/java/com/zpark/ssm/App.java
@@ -0,0 +1,9 @@
+package com.zpark.ssm;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello.Git!");
+        System.out.println("Hello.Git! thz master second commit!");
+        System.out.println("Hello Git! thz hotfix first commit!");
+    }
+}
